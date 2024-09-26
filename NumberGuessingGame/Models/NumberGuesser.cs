3d30ffefc232ac4e3bf3fbbc549c2e8bcc0d2b8e
Pranay Raut <pranayraut129@gmail.com>
2024-09-26 14:16:25 +0530

    Add project files.

---
+++ NumberGuessingGame/Models/NumberGuesser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame.Models
+{
+    internal class NumberGuesser
+    {
+        public static string NumberGuessing(int number, int guess)
+        {
+            if (guess == number)
+            {
+                return "correct";
+            }
+            else if (guess < number)
+            {
+                return "Too low";
+            }
+            else
+            {
+                return "Too high";
+            }
+        }
+    }
+}
---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,52 @@
+﻿using NumberGuessingGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int low = 1;
+            int high = 100;
+            int maxChances = 10;    
+            int chance = 0;         
+            bool win = false;      
+            
+
+            Console.WriteLine("Welcome to the Number Guessing Game!");
+
+            Random random = new Random();
+            int number = random.Next(low, high);
+
+            while (maxChances != chance)
+            {
+                Console.WriteLine($"Guess the number within {low} to {high}: ");
+                int  guess= int.Parse(Console.ReadLine()); 
+
+                string output = NumberGuesser.NumberGuessing(number, guess);  
+                chance++;  
+
+                if (output == "correct")
+                {
+                    Console.WriteLine($"Congratulations! You guessed the correct number in {chance} chances.");
+                    win = true;
+                    break;  
+                }
+                else
+                {
+                    Console.WriteLine($"{output}. Try again\n");
+                }
+            }
+
+            if (!win)
+            {
+                Console.WriteLine($"Sorry, you ran out of chances. The correct number was {number}.");
+            }
+        }
+    }
+}
